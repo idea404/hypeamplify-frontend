682524da579d4dd4c32c008dbbcadf58b41e72a4
idea404 <dennis.oliver.huisman@gmail.com>
2025-03-25 19:01:03 +0100

    path resolving for next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     // Warning: This disables ESLint checking during build
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
   },
 };
 
